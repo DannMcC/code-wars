69f2b1a9ec8a75b0d282144abdf625ea86b36e64
Dan McCune <megadannn@gmail.com>
2016-12-26 16:10:38 -0500

    link

---
+++ JSkata/growth_of_a_population.js
@@ -0,0 +1,10 @@
+// 7 kyu
+function nbYear (p0, percent, aug, p) {
+  let nPop = p0 + (p0 * (percent / 100)) + aug
+  let year = 1
+  for (let i = 0; nPop < p; i++) {
+    nPop = nPop + (nPop * (percent / 100)) + aug
+    year += 1
+  }
+  return year
+}
