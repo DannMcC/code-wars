1eb1ce02ec541a859cd1113bff98f1790a643020
Dan McCune <megadannn@gmail.com>
2017-01-16 18:49:54 -0500

    multiples of 3 and 5

---
+++ JSkata/Multiples_of_3_and_5.js
@@ -0,0 +1,14 @@
+const solution = number =>{
+let sum = 0
+  for (let i = 0; i < number; i++) {
+    if (i % 15 ===0) {
+      sum +=i
+    }
+    else if (i % 3 ===0){
+      sum +=i
+    }
+    else if (i % 5 ===0){
+      sum += i
+    }
+  } return sum
+}
