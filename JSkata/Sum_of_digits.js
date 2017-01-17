21b3e3b786f282c9f79a46f05262c58fc16a6229
Dan McCune <megadannn@gmail.com>
2017-01-16 19:18:13 -0500

    refactored with arrow function

--- JSkata/Sum_of_digits.js
+++ JSkata/Sum_of_digits.js
@@ -4,3 +4,9 @@ function digital_root(n) {
   return newn.toString().split('').length ===1 ? Number(newn) : digital_root(newn)
 
 }
+
+const digital_root = n => {
+  let newn = n.toString().split('').reduce(function(a, b) {
+    return Number(a) + Number(b)})
+  return newn.toString().split('').length ===1 ? Number(newn) : digital_root(newn)
+}
